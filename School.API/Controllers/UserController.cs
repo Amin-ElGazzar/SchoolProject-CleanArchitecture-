80ed73d0aebfb2e001496eb1b8ec2eacadfd1bdd
Amin ElGazzar <aminelgazzar0@gmail.com>
2024-06-18 17:34:00 +0300

    handle user get all and user get by id

---
+++ School.API/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using School.API.Base;
+using School.Application.Features.User.Query.Models.Request;
+
+namespace School.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerMain
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _mediator.Send(new UserGetAllRequest());
+            return GetResponse(result);
+        }
+
+        [HttpGet("id/{id}")]
+        public async Task<IActionResult> Get([FromRoute] string id)
+        {
+            var result = await _mediator.Send(new UserGetByIdRequest() { Id = id });
+            return GetResponse(result);
+        }
+    }
+}
---
+++ School.Application/Features/User/Query/Models/responses/UserGetAllResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace School.Application.Features.User.Query.Models.responses
+{
+    public class UserGetAllResponse
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
