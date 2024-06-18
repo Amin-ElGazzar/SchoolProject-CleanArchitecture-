aeddee26b25e01ea9a54b494902a2850065ca3f4
Amin ElGazzar <aminelgazzar0@gmail.com>
2024-06-18 12:56:28 +0300

    handle image and document

--- School.Application/Common/Extensions/IQueryableExtension.cs
+++ School.Application/Common/Extensions/IQueryableExtension.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using School.Application.Common.Models;
 
-namespace School.Application.Common.Models
+namespace School.Application.Common.Extensions
 {
     public static class IQueryableExtension
     {
