f9447342f1f13a751607a2a156649fd0111310ba
Amin ElGazzar <aminelgazzar0@gmail.com>
2024-06-19 19:28:17 +0300

    soft delete for student

--- School.Presistence/Repositories/StudentRepo.cs
+++ School.Presistence/Repositories/StudentRepo.cs
@@ -2,25 +2,26 @@
 using School.Application.Contracts.Repositories;
 using School.Domain.Entities;
 using School.Presistence.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace School.Presistence.Repositories
 {
-    public class StudentRepo :BaseRepo<Student>,IStudentRepo
+    public class StudentRepo : BaseRepo<Student>, IStudentRepo
     {
         private readonly ApplicationDbContext _context;
 
-        public StudentRepo(ApplicationDbContext context):base(context)
+        public StudentRepo(ApplicationDbContext context) : base(context)
         {
-           _context = context;
+            _context = context;
         }
         public override async Task<IEnumerable<Student>> GetAllAsync()
         {
-            return await _context.Student.Include(x=>x.Department).ToListAsync() ;
+            return await _context.Student.Include(x => x.Department).ToListAsync();
+        }
+
+        public override void Delete(Student entity)
+        {
+            entity.IsDeleted = true;
+            _context.Update(entity);
         }
     }
 }
